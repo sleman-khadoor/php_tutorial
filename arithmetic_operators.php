bb570fb4ce84be6b4c4c5f61a9a5974a8b8d385a
sleman-khadoor <slemankhadoorit@gmail.com>
2022-07-09 02:29:07 +0300

    arithmetic operators

---
+++ arithmetic_operators.php
@@ -0,0 +1,24 @@
+<?php
+$num1 =10;
+$num2 =2;
+
+// addition +
+$resault1 =$num1 + $num2;
+echo $resault1.'<br>';
+
+// substraction -
+$resault2 =$num1 - $num2;
+echo $resault2.'<br>';
+
+// multiplication *
+$resault3 =$num1 * $num2;
+echo $resault3.'<br>';
+
+// division /
+$resault4 =$num1 / $num2;
+echo $resault4.'<br>';
+
+// modular %
+$resault5 =$num1 % $num2;
+echo $resault5.'<br>';
+?>
