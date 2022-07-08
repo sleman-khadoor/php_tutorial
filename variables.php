1ea063b60cec78bf5d820a3e979d24c51325e86e
sleman-khadoor <slemankhadoorit@gmail.com>
2022-07-09 00:59:05 +0300

    variables

---
+++ variables.php
@@ -0,0 +1,10 @@
+<?php
+// variable names start with $
+// it can't start with a number
+// but it can start with _
+$name = 'Sleman';
+$age = 28;
+
+echo "My name is $name, and I am $age years old";
+echo '<br>My name is '.$name.', and I am '.$age.' years old';
+?>
