0332ec7947c504a74f77b0c7f68db31d0d040c22
sleman-khadoor <slemankhadoorit@gmail.com>
2022-07-09 02:38:37 +0300

    comparasion operators

---
+++ comparasion_operators.php
@@ -0,0 +1,23 @@
+<?php
+// ==   !=   >   <   >=   <= 
+$name = 'sleman';
+$age =28;
+
+if($name == 'ali')
+{
+    echo '== work ok'.'<br>';
+}
+if($name != 'ali')
+{
+    echo '!- work ok'.'<br>';
+}
+if($age > 2)
+{
+    echo '> work ok'.'<br>';
+}
+
+if($age < 1)
+{
+    echo '< work ok'.'<br>';
+}
+?>
