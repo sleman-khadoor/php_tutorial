84d4a936b3159af2d6c89e681923ee0747b9fe4d
sleman-khadoor <slemankhadoorit@gmail.com>
2022-07-09 03:23:47 +0300

    switch statement

---
+++ switch_statement.php
@@ -0,0 +1,19 @@
+<?php
+$num = 10;
+
+switch($num)
+{
+    case 1:
+        echo '1';
+        break;
+    case 3:
+        echo 3;
+        break;
+    case 10:
+        echo 10;
+        break;
+    default:
+        echo 'nuber not recognized';
+
+}
+?>
